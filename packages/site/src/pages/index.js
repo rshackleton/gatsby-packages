a2ccc127e1d30ebe5ba9aa77f15403cc3f04d6b9
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2020-02-05 21:33:19 +0000

    Add gatsby image example

--- packages/site/src/pages/index.js
+++ packages/site/src/pages/index.js
@@ -1,3 +1,40 @@
+import Image from "gatsby-image";
 import React from "react";
+import { graphql } from "gatsby";
 
-export default () => <div>Homepage in a user's site</div>;
+export default ({ data }) => {
+  return (
+    <div style={{ margin: "0 auto", maxWidth: "500px" }}>
+      <Image
+        fluid={
+          data.allKontentItemHomePage.nodes[0].elements.background_image
+            .value[0].fluid
+        }
+      />
+    </div>
+  );
+};
+
+export const query = graphql`
+  query HomePageQuery {
+    allKontentItemHomePage {
+      nodes {
+        elements {
+          background_image {
+            value {
+              fluid(maxWidth: 500) {
+                aspectRatio
+                base64
+                sizes
+                src
+                srcSet
+                srcSetWebp
+                srcWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
