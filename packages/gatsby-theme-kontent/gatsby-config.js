43724dd08b9591bae02646453c230579b333cf8f
Richard Shackleton <richardshackleton@netconstruct.co.uk>
2020-02-05 21:22:01 +0000

    Add plugins to gatsby theme

--- packages/gatsby-theme-kontent/gatsby-config.js
+++ packages/gatsby-theme-kontent/gatsby-config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = options => ({
+  plugins: [
+    {
+      resolve: `@kentico/gatsby-source-kontent`,
+      options: options.kontent || {}
+    },
+    {
+      resolve: `@rshackleton/gatsby-transformer-kontent-image`,
+      options: options.kontentImage || {}
+    }
+  ]
+});
--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -1,3 +1,22 @@
+require("dotenv").config();
+
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-kontent`, options: {} }]
+  plugins: [
+    {
+      resolve: `@rshackleton/gatsby-theme-kontent`,
+      options: {
+        kontent: {
+          deliveryClientConfig: {
+            projectId: process.env.KC_PROJECT_ID,
+            typeResolvers: [],
+            previewApiKey: process.env.KC_PREVIEW_KEY,
+            globalQueryConfig: {
+              usePreviewMode: !!process.env.KC_PREVIEW_KEY
+            }
+          },
+          languageCodenames: [`default`]
+        }
+      }
+    }
+  ]
 };
